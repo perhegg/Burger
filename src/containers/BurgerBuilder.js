76414ed26d3c0a571d7a09d99dcd114123ec92eb
Per Eriksson <per.eriksson@trainimal.com>
2020-11-26 18:45:45 +0100

    Add Build controls

--- src/containers/BurgerBuilder.js
+++ src/containers/BurgerBuilder.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Burger from '../components/Burger/Burger';
+import BuildControls from '../components/Burger/BuildControls/BuildControls'
 
 export default class BurgerBuilder extends Component {
     state = {
@@ -13,7 +14,10 @@ export default class BurgerBuilder extends Component {
 
     render() {
         return (
-            <Burger ingredients={this.state.ingredients} />
+            <>
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls />
+            </>
         )
     };
 };
