bccb571558f8b349ba5ebff1ca7d8bf5059850ca
Per Eriksson <per.eriksson@trainimal.com>
2021-01-14 10:06:38 +0100

    add checkout summary box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout'
 
 function App() {
   return (
     <div className="App">
       <Layout>
         <BurgerBuilder/>
+        <Checkout />
       </Layout>
     </div>
   );
