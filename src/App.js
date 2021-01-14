683a0400623b3f9639e84730758c84017d844a16
Per Eriksson <per.eriksson@trainimal.com>
2021-01-14 10:23:07 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout'
 
-function App() {
-  return (
-    <div className="App">
-      <Layout>
-        <BurgerBuilder/>
-        <Checkout />
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  render () {
+    return (
+      <div className="App">
+        <Layout>
+          <Switch>
+            <Route path="/checkout" component={ Checkout } />
+            <Route path="/" component={ BurgerBuilder } />
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
