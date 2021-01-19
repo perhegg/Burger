38ad3dfac5a4a5d105915ff39809754b025eb6ac
Per Eriksson <per_hegg@hotmail.com>
2021-01-19 21:17:40 +0100

    install redux

--- src/index.js
+++ src/index.js
@@ -4,14 +4,21 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-  
-  const app = (
+import { Provider } from "react-redux"
+import reducer from "./store/reducer"
+import { createStore } from "redux"
+
+const store = createStore(reducer)
+
+const app = (
+  <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  )
+  </Provider>
+)
 
-ReactDOM.render( app, document.getElementById('root')
+ReactDOM.render(app, document.getElementById('root')
 );
 
 reportWebVitals();
