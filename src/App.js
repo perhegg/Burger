5a5abb81aec8fa4ff7d51f67c7e9b72a0acbdd2c
Per Eriksson <per_hegg@hotmail.com>
2021-01-26 22:01:41 +0100

    add auth actions

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout'
 import Orders from './containers/Orders/Orders'
+import Auth from './containers/Auth/Auth'
 
 class App extends Component {
   render () {
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
         <Layout>
           <Switch>
+            <Route path="/auth" component={ Auth } />
             <Route path="/checkout" component={ Checkout } />
             <Route path="/orders" component={ Orders } />
             <Route path="/" component={ BurgerBuilder } />
